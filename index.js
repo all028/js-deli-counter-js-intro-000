c495c36c8293dc30c438102a0a031050831eb87c
al <albliu@email.com>
2019-11-25 01:25:42 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function takeANumber(line, name){
+
+
+  var rsltStr = "Welcome " + name + ".  You are number " +  line + " in line";
+  return rsltStr;
+}//end takeANumber
+
+
+function nowServing(num){
+
+
+}//end nowServing
+
+function currentLine(num){
+
+
+}//end currentLine
