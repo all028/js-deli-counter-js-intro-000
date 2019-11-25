ab3eeed012ac2f136ca3048ad9e4fc0fd2f0dfc0
al <albliu@email.com>
2019-11-25 01:43:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function takeANumber(line, name){
   var rsltStr = "";
   for (let x = 0; x <= name.length -1;x++){
     rsltStr = "Welcome " + name + ".  You are number " +  line + " in line";
+    line++;
   }
 
 //  return rsltStr;
