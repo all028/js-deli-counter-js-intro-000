1bb8c0bc35bac56c91d8b4491a5b9ddebc727832
al <albliu@email.com>
2019-11-25 02:23:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function nowServing(line){
 
 function currentLine(line){
     if (line.length ==0){
-      return "There line is currently empty.";
+      return "The line is currently empty.";
     }
 
     var tempStr = "The line is currently:"
