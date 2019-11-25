77f941cc1d837fc189dc3fee184b103b04a18acd
al <albliu@email.com>
2019-11-25 02:48:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,7 +30,8 @@ function currentLine(line){
     var j = 1
     for (let i = 0; i < line.length-1; i++){
       var k = j.toString();
-      addStr += " " + k + ". " + line[i] + ", ";
+      addStr += " " + k + ". " + line[i] + ",";
+      j++;
     }
 
     var result = "The line is currently:" + addStr;
